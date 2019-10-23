acc5fb522c62e7dea73568348fb1b9e153468a4c
ryotaro-kitamura <k.ryoutaro.g@gmail.com>
2019-10-23 13:00:20 +0900

    表示させるまでやった

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
 import React from 'react';
-import ApolloClient from 'apollo-boost'
-import { ApolloProvider } from 'react-apollo'
-import logo from './logo.svg';
-import './App.css';
-
-const client = new ApolloClient({
-  uri: "https://vm8mjvrnv3.lp.gql.zone/graphql"
-});
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag'
+ 
+const query = gql`
+  {
+    viewer { 
+      name
+      repositories(first: 3){
+        edges{
+          node{
+            name
+            url
+          }
+        }
+      }
+    }
+  }
+`
 
 const App = () => (
-  <ApolloProvider client={client}>
-    <div>
-      <h2>My first Apollo React Application</h2>
-    </div>
-  </ApolloProvider>
-)
+  <Query query={query}>
+    {({loading, error, data}) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>{error.toString()}</p>;
 
+      console.log(data);
+
+      const name = data.viewer.name
+      const repositories = data.viewer.repositories.edges
+
+      return (
+        <div style={{textAlign: "center"}}>
+          <h2>{name}のGithub</h2>
+          <h3>最初の3つのRepositoryを取得してみたよ!</h3>
+          <ul>
+            {repositories.map(r => (
+              <li key={r.node.id} style={{listStyle: "none"}}>
+                <a href={r.node.url}>{r.node.name}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )
+    }}
+  </Query>
+)
+ 
 export default App;
+ 
